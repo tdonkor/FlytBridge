de856b9c61b7ca2e19d6c6ce5b47768914eb0c63
Dev PC <Dev PC@TERM98>
2019-12-11 15:42:22 +0000

    Add project files.

---
+++ GenericPOSRestService.Common/ServiceCallClasses/RestCalls.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+
+using System.Net.Http.Headers;
+using RestSharp;
+
+namespace GenericPOSRestService.Common.ServiceCallClasses
+{
+    public class RestCalls
+    {
+
+        //call 1
+        public string PostCheckBasket()
+        {
+            return string.Empty;
+        }
+        //call 2
+        public string PostFulfillment()
+        {
+            return string.Empty;
+        }
+        //call3
+        public string MarkAsPaid()
+        {
+            return string.Empty;
+        }
+        //call4
+
+        public string Order()
+        {
+            return string.Empty;
+        }
+        //call5
+
+        public string SendToPost()
+        {
+            return string.Empty;
+        }
+    }
+}
